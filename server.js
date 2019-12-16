41e2c45bdcd5361d3e7132edae5fcf740043a6c6
Mary Jacobs <TheMaryJacobs@gmail.com>
2019-12-16 16:53:15 -0600

    set up and pseudocode server.js

---
+++ server.js
@@ -0,0 +1,42 @@
+// List depenencies
+
+
+
+// Set the port
+
+
+
+// Set up express
+
+
+
+//Create routes for HTMLa
+    // * GET `/notes` -  `notes.html` file.
+
+    // * GET `*` -`index.html` file
+
+
+
+// static thing we talked about in class
+
+app.use(express.static('public'))
+
+
+//make requests
+
+
+
+//get function
+
+
+
+//post function
+
+
+
+//delete function
+
+
+
+
+//start server
